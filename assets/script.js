394afb30c20948a804da082faf9863853dff8a86
jeremy-fong <112743562+jeremy-fong@users.noreply.github.com>
2022-10-26 23:58:15 -0700

    added code to fetch data for city input and append it to the page

--- assets/script.js
+++ assets/script.js
@@ -53,3 +53,39 @@ $('#searchbtn').on('click', function(e) {
     
 })
 
+// get coordinates for city and to fetch the data
+function getApi(cityInput) {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=5&appid=98dab0374d715d8b0802f2824300c3e2`)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+
+        currentWeather(data[0].lat,data[0].lon)
+    })
+
+}
+
+// get the data for the city input 
+function currentWeather(lat,lon){
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=98dab0374d715d8b0802f2824300c3e2&units=imperial`)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+
+        cityEl.text(data.name + "           ")
+        currentTempEl.text(data.main.temp)
+        currentFeelsEl.text(data.main.feels_like)
+        currentHumidityEl.text(data.main.humidity)
+        currentWindEl.text(data.wind.speed)
+        weatherIconEl.attr(
+            'src',
+            `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+        ).attr('alt', data.weather[0].description);
+    })
+
+
+}
